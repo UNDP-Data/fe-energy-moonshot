d160be6e96039eba8922b32804b0745139969549
Alice Feng <feng.a.g@gmail.com>
2022-04-28 17:02:51 -0400

    Remove all other graph types

--- src/GrapherComponent/index.tsx
+++ src/GrapherComponent/index.tsx
@@ -2,9 +2,7 @@ import { useContext, useState } from 'react';
 import styled from 'styled-components';
 import { Modal } from 'antd';
 import { CtxDataType, DataType, IndicatorMetaDataWithYear } from '../Types';
-import {
-  ScatterPlotIcon, BarGraphIcon, MapIcon, DualAxesChartIcon, MultiLineChartIcon, Logo,
-} from '../Icons';
+import { Logo } from '../Icons';
 import Context from '../Context/Context';
 import { Settings } from './Settings';
 import { Graph } from './Graph';
@@ -31,11 +29,6 @@ const RootEl = styled.div`
   box-shadow: var(--shadow);
 `;
 
-const TabsContainerEl = styled.div`
-  background-color: var(--black-200);
-  display: flex;
-`;
-
 const HeadingEl = styled.div`
   display: flex;
   justify-content: space-between;
@@ -43,56 +36,9 @@ const HeadingEl = styled.div`
   align-items: center;
 `;
 
-interface SelectedData {
-  selected?: boolean;
-}
-
 const TitleEl = styled.div`
   display: flex;
   align-items: center;
-`;
-
-const TabsEl = styled.div<SelectedData>`
-  font-size: 1.2rem;
-  padding: 1rem 0;
-  line-height: 1.4rem;
-  min-width: 10rem;
-  width: 15%;
-  text-transform: uppercase;
-  justify-content: center;
-  background-color:${(props) => (props.selected ? 'var(--white)' : 'transparent')};
-  color:${(props) => (props.selected ? 'var(--primary-blue)' : 'var(--black-600)')};
-  text-align: center;
-  border-right: 1px solid var(--black-450);
-  opacity :${(props) => (props.selected ? 1 : 0.6)};
-  cursor: pointer;
-  div{
-    margin-bottom: 0.5rem;
-  }
-  &:hover {
-    opacity: 1;
-  }
-  @media (max-width: 1172px) {
-    width: 20%;
-    &:last-of-type {
-      border-right: 0 solid var(--black-450);
-    }
-  }
-  @media (max-width: 900px) {
-    width: fit-content;
-    min-width: 0;
-    padding: 1rem 2rem;
-    &:last-of-type {
-      border-right: 1px solid var(--black-450);
-    }
-  }
-  @media (max-width: 700px) {
-    font-size: 1rem;
-    width: 100%;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
 `;
 
 const GraphEl = styled.div`
@@ -101,13 +47,6 @@ const GraphEl = styled.div`
   @media (max-width: 960px) {
     display: inline;
   }  
-`;
-
-const IconEl = styled.div`
-  display: inline;
-  @media (max-width: 980px) {
-    display: none;
-  }
 `;
 
 const ButtonsEl = styled.div`
@@ -147,9 +86,7 @@ export const GrapherComponent = (props: Props) => {
     countries,
   } = props;
   const {
-    graphType,
     showSource,
-    updateGraphType,
   } = useContext(Context) as CtxDataType;
   const [modalVisibility, setModalVisibility] = useState(false);
   const queryParams = new URLSearchParams(window.location.search);
@@ -179,43 +116,6 @@ export const GrapherComponent = (props: Props) => {
           </ButtonsEl>
         </HeadingEl>
         <RootEl>
-          {
-            queryParams.get('showSettings') === 'false' ? null
-              : (
-                <TabsContainerEl>
-                  <TabsEl selected={graphType === 'map'} onClick={() => { updateGraphType('map'); }}>
-                    <IconEl>
-                      <MapIcon size={48} fill={graphType === 'map' ? 'var(--primary-blue)' : 'var(--black-500)'} />
-                    </IconEl>
-                    <>Maps</>
-                  </TabsEl>
-                  <TabsEl selected={graphType === 'scatterPlot'} onClick={() => { updateGraphType('scatterPlot'); }}>
-                    <IconEl>
-                      <ScatterPlotIcon size={48} fill={graphType === 'scatterPlot' ? 'var(--primary-blue)' : 'var(--black-500)'} />
-                    </IconEl>
-                    <>Correlation</>
-                  </TabsEl>
-                  <TabsEl selected={graphType === 'barGraph'} onClick={() => { updateGraphType('barGraph'); }}>
-                    <IconEl>
-                      <BarGraphIcon size={48} fill={graphType === 'barGraph' ? 'var(--primary-blue)' : 'var(--black-500)'} />
-                    </IconEl>
-                    <>Ranks</>
-                  </TabsEl>
-                  <TabsEl selected={graphType === 'trendLine'} onClick={() => { updateGraphType('trendLine'); }}>
-                    <IconEl>
-                      <DualAxesChartIcon size={48} fill={graphType === 'trendLine' ? 'var(--primary-blue)' : 'var(--black-500)'} />
-                    </IconEl>
-                    <>Dual Axes Line Chart</>
-                  </TabsEl>
-                  <TabsEl selected={graphType === 'multiCountryTrendLine'} onClick={() => { updateGraphType('multiCountryTrendLine'); }}>
-                    <IconEl>
-                      <MultiLineChartIcon size={48} fill={graphType === 'multiCountryTrendLine' ? 'var(--primary-blue)' : 'var(--black-500)'} />
-                    </IconEl>
-                    <>Multi Country Trends</>
-                  </TabsEl>
-                </TabsContainerEl>
-              )
-          }
           <GraphEl>
             {
               queryParams.get('showSettings') === 'false' ? null
@@ -239,7 +139,6 @@ export const GrapherComponent = (props: Props) => {
                 <Graph
                   data={data}
                   indicators={indicators}
-                  countries={countries}
                   fullWidth={queryParams.get('showSettings') === 'false'}
                 />
               )
