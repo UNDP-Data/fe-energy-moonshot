5303597800dd1448566fccccdffaf8f832276f92
Антон Степаненков <lenseg1@gmail.com>
2024-03-17 22:44:23 +0300

    wip: stacked charts

--- src/Components/StackedChart.tsx
+++ src/Components/StackedChart.tsx
@@ -1,64 +1,49 @@
+import { useEffect, useState } from 'react';
 // @ts-ignore:next-line
 import StackedBarChart from 'stacked-barchart';
 
-// interface BarchartData {
-// }
-
 interface Props {
-  // data: BarchartData;
+  data: any;
+  id: string;
+  clickCallback: Funcion;
 }
 
 export default (props: Props) => {
-  // const {
-  //   data,
-  // } = props;
+  const {
+    data,
+    id,
+    clickCallback
+  } = props;
 
-  const graph = StackedBarChart(
-      { data: [
-        {
-          "SIDS": {
-            "color":"#AEFFEC",
-            "value":15000,
-            "order":1
-          },
-          "LDCS": {
-            "color":"#55ECD1",
-            "value":10000,
-            "order":2,
-            "overlap":3000
-          },
-          "LLDCS": {
-            "color":"#00D2E0",
-            "value":50000,
-            "order":3,
-            "overlap":5000
-          },
-          "Other": {
-            "color":"#03C38A",
-            "value":20000,
-            "order":4
-          }
-        }
-      ]
-    },
+  const [graph, setGraph] = useState()
+
+  useEffect(() => {
+    const graph = StackedBarChart(
       {
-        containerSelector: "#chart-container"
-      }
+        data: [
+          data,
+        ],
+      },
+      {
+        containerSelector: `#${id}`,
+        width: document.getElementById(id)?.offsetWidth,
+        height: 60,
+        margin: { left: 0, top: 0, bottom: 0 },
+      },
     );
+    setGraph(graph);
+    graph.on('nodeClick', (event) => {
+      clickCallback(event.clickedNodeData.key)
+    })
+  }, []);
 
-    // To expand the graph upon node click (ie. to see more connections another hop away)
-    // The clicked data object can also be used to extract new information
-    // graph.on('nodeClick', (event) => {
-    //   const node = event.clickedNodeData
-    //   const newNode = data1[node.row][node.key]
-    //   newNode.key = node.key
-    //   newNode.row = node.row
-    //   graph.update(newNode)
-    // })
+  // useEffect(() => {
+  //   if(graph) {
+  //     graph.updateAll(data)
+  //   }
+  // }, [data])
 
   return (
-    <div id='chart-container'>
-
-    </div>
+    <div id={id} />
   );
 };
--- src/Global/Settings.tsx
+++ src/Global/Settings.tsx
@@ -1,33 +1,20 @@
 import { useContext, useEffect, useState } from 'react';
-import { Select, Segmented, Tooltip } from 'antd';
+import { Segmented } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { CtxDataType } from '../Types';
 import Context from '../Context/Context';
-import {
-  outputsTaxonomy, countryGroupingsTaxonomy, genderMarkers, fundingTaxonomy,
-} from '../Constants';
+import { outputsTaxonomy } from '../Constants';
 
-interface Props {
-  countryList: string[];
-}
-
-export const Settings = (props: Props) => {
+export const Settings = () => {
   const {
-    selectedRegions,
-    selectedFunding,
-    selectedGenderMarker,
     selectedCategory,
     selectedSubCategory,
-    updateSelectedRegions,
-    updateSelectedFunding,
-    updateSelectedGenderMarker,
     updateSelectedCategory,
     updateSelectedSubCategory,
   } = useContext(Context) as CtxDataType;
   // translation
   const { t } = useTranslation();
 
-  const { countryList } = props;
 
   const outputsTaxonomyTranslated = outputsTaxonomy.map((ot) => ({
     value: ot.value,
@@ -42,16 +29,6 @@ export const Settings = (props: Props) => {
     const activeOutputsTaxonomy = outputsTaxonomyTranslated.find((category) => category.value === selectedCategory)
       || outputsTaxonomyTranslated[0];
     return activeOutputsTaxonomy?.subcategories;
-  });
-
-  const countryGroupingsMerged = [...countryGroupingsTaxonomy];
-  countryGroupingsMerged.push({
-    label: 'countries',
-    key: 'countries',
-    options: countryList.map((c) => ({
-      label: c,
-      value: c,
-    })),
   });
 
   useEffect(() => {
@@ -99,108 +76,6 @@ export const Settings = (props: Props) => {
             value={selectedSubCategory}
             options={subCategoriesTaxonomy}
           />
-        </div>
-      </div>
-      <div className='flex-div flex-space-between margin-bottom-07'>
-        <div style={{ maxWidth: 'calc(33.33% - .65rem)', width: '100%' }}>
-          <Tooltip
-            title={<div dangerouslySetInnerHTML={{ __html: t('country-group-tooltip') || '' }} />}
-            placement='top'
-            overlayStyle={{
-              maxWidth: '550px',
-            }}
-          >
-            <p className='label underline'>{t('select-country-group')}</p>
-          </Tooltip>
-          <Select
-            showSearch
-            className='undp-select'
-            filterOption={(input, option) => (option?.label ?? '').toString().toLowerCase().includes(input?.toLowerCase())}
-            placeholder={t('select-country-group')}
-            value={selectedRegions}
-            onChange={(d: string) => { updateSelectedRegions(d === undefined ? 'all' : d); }}
-          >
-            {
-              countryGroupingsMerged.map((d) => {
-                if (d.options) {
-                  return (
-                    <Select.OptGroup key={d.key} label={t(d.label)}>
-                      {d.options.map((o) => (
-                        <Select.Option
-                          className='undp-select-option'
-                          label={t(o.label)}
-                          key={o.value}
-                        >
-                          {t(o.label)}
-                        </Select.Option>
-                      ))}
-                    </Select.OptGroup>
-                  );
-                }
-                return (
-                  <Select.Option className='undp-select-option' label={t(d.label)} key={d.value}>{t(d.label)}</Select.Option>
-                );
-              })
-            }
-          </Select>
-        </div>
-        <div style={{ maxWidth: 'calc(33.33% - .65rem)', width: '100%' }}>
-          <Tooltip
-            title={<div dangerouslySetInnerHTML={{ __html: t('gender-marker-tooltip') || '' }} />}
-            placement='top'
-            overlayStyle={{
-              maxWidth: '550px',
-            }}
-          >
-            <p className='label underline'>{ t('select-gender-marker')}</p>
-          </Tooltip>
-          <Select
-            showSearch
-            className='undp-select'
-            filterOption={(input, option) => (option?.label ?? '').toString().toLowerCase().includes(input?.toLowerCase())}
-            placeholder={t('select-taxonomy')}
-            value={selectedGenderMarker}
-            onChange={(d: string) => { updateSelectedGenderMarker(d === undefined ? 'all' : d); }}
-          >
-            {
-              genderMarkers.map((d) => (
-                <Select.Option className='undp-select-option' label={t(d.label)} key={d.value}>
-                  { d.tooltip ? (
-                    <Tooltip title={t(d.tooltip)}>
-                      {t(d.label)}
-                    </Tooltip>
-                  ) : (
-                    t(d.label)
-                  )}
-                </Select.Option>
-              ))
-            }
-          </Select>
-        </div>
-        <div style={{ maxWidth: 'calc(33.33% - .65rem)', width: '100%' }}>
-          <Tooltip
-            title={<div dangerouslySetInnerHTML={{ __html: t('funding-tooltip') || '' }} />}
-            placement='top'
-            overlayStyle={{
-              maxWidth: '550px',
-            }}
-          >
-            <p className='label underline'>{t('select-funding')}</p>
-          </Tooltip>
-          <Select
-            showSearch
-            filterOption={(input, option) => (option?.label ?? '').toString().toLowerCase().includes(input?.toLowerCase())}
-            className='undp-select'
-            placeholder={t('select-funding')}
-            value={selectedFunding}
-            onChange={(d: string) => { updateSelectedFunding(d === undefined ? 'all' : d); }}
-          >
-            {
-              fundingTaxonomy.map((d) => (
-                <Select.Option className='undp-select-option' label={t(d.label)} key={d.value}>{t(d.label)}</Select.Option>
-              ))
-            }
-          </Select>
         </div>
       </div>
     </div>
